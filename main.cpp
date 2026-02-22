74a49653fa42bf2b13c4e08a335a8166248bc75f
Jhony279 <lordjhony732@gmail.comm>
2026-02-21 23:03:59 -0800

    Writing first function using arrays

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 // COMSC-210 | Lab 13 | Johnathan Perez Baltazar
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>    // for sort(), find(), max_element(), min_element()
 #include <numeric>  // for accumulate()
 using namespace std;
 
+const int MAX_DAYS = 30;
 const string FILE_LOCATION = " ";
 
-void populateArray(string, vector<double>&);
+void populateArray(string, vector<double>&, vector<string>&, vector<int>&);
 
 /**
  * @brief - Description of main
@@ -16,4 +18,16 @@ void populateArray(string, vector<double>&);
 */
 int main() {
     return 0;
+}
+
+void populateArray(string fLocation, vector<double>& tVector, vector<string>& invArray, 
+                    vector<int>& invLineArray){
+    ifstream fileData;
+    string text;
+
+    fileData.open(fLocation);
+    if (fileData.good()){
+        static int i = 0;
+        while(getline(fileData, text) && i < MAX_DAYS){}
+    }
 }
